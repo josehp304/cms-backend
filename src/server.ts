c8434ef60cb1fcbda1f87c318e25b1adcaee23e0
joseph babu <josehp304@gmail.com>
2026-01-22 01:35:36 +0530

    fix: comment out allowedHeaders in CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
     // credentials: true
   })
 );  
