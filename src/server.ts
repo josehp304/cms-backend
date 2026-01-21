75f85e387919c5e87537e2dd8f9c87369f83c52c
joseph babu <josehp304@gmail.com>
2026-01-22 01:11:25 +0530

    fix for cors error

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(
-  cors()
+  cors({
+    origin: process.env.ALLOWED_HOSTS ? process.env.ALLOWED_HOSTS.split(',') : 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
